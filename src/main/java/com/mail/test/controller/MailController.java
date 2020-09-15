dac1b2711521b8cf34c96664427795c1b1abdd44
WooSungHwan <doqndnffo@gmail.com>
2020-09-15 13:26:42 +0900

    이메일 발송 gradle 예제

---
+++ src/main/java/com/mail/test/controller/MailController.java
@@ -0,0 +1,26 @@
+package com.mail.test.controller;
+
+import com.mail.test.dto.MailDto;
+import com.mail.test.service.MailService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@AllArgsConstructor
+public class MailController {
+    private final MailService mailService;
+
+    @GetMapping("/mail")
+    public String dispMail() {
+        return "mail";
+    }
+
+    @PostMapping("/mail")
+    public void execMail(MailDto mailDto) {
+        long start = System.currentTimeMillis();
+        mailService.mailSend(mailDto);
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
---
+++ src/main/java/com/mail/test/util/MailHandler.java
@@ -0,0 +1,4 @@
+package com.mail.test.util;
+
+public class MailHandler {
+}
